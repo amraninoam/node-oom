dc8f9853c3e4d411681f21aa8adc62ec22c283a4
Noam Amrani <noama@sela.co.il>
2021-06-23 11:58:48 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(8000, function () {
+  console.log('Listening to Port 8000');
+});
+
+
+const list = [];
+setInterval(()=> {
+  const record = new MyRecord();
+  list.push(record);
+},10);
+
+function MyRecord() {
+  var x='hii';
+  this.name = x.repeat(10000000);
+  this.id = x.repeat(10000000);
+  this.account = x.repeat(10000000);
+}
+setInterval(()=> {
+  console.log(process.memoryUsage())
+},100);
